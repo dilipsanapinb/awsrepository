a7d68da76bc104fa5ed91a5236b57399d4b44963
Dilip Sanap <112867608+dilipsanapinb@users.noreply.github.com>
2023-03-17 21:48:55 +0530

    AWS_!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    console.log("Hello world");
+    res.send("Hello world");
 })
 
 app.listen(8000, () => {
