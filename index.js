4feab174ca71bb71faea2ff5e14a30dfa7bfd185
Dilip Sanap <112867608+dilipsanapinb@users.noreply.github.com>
2023-03-17 22:15:23 +0530

    aa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req, res) => {
-    res.send("welcome to server1");
+    res.send("welcome to server-1");
 })
 
 app.listen(8000, () => {
